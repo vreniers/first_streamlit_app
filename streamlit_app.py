baec88daa25c8e00ef1e9af7e38734725a208950
Vincent Reniers <vincent.reniers@gmail.com>
2023-01-17 10:37:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.text('Omega 3 & Bueberry Oatmmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
