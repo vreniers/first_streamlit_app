01850f519bfef867e7c3e7c6e4ecf98bbb815c42
Vincent Reniers <vincent.reniers@gmail.com>
2023-01-17 10:44:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,8 @@ streamlit.text('🥑🍞 AVocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
