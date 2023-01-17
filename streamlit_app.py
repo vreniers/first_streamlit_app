e189fd9d62f31bc94dda8faef01bd9befa102f46
Vincent Reniers <vincent.reniers@gmail.com>
2023-01-17 10:39:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,5 @@ streamlit.text('🥣 Omega 3 & Bueberry Oatmmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 AVocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
